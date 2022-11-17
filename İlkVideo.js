fbfd22bffb593eab7d37a94a7bc110611c1ee55b
Beyzavaroldemir <118390913+beyzavaroldemir@users.noreply.github.com>
2022-11-17 07:46:12 +0000

    Initial commit

---
+++ İlkVideo.js
@@ -0,0 +1,17 @@
+console.log("Hello World");
+let dolarbugun = 9.30
+let dolardun = 9.2;
+console.log(dolardun);
+
+const euroDun = 11.3;
+// euroDun=12; // Bu değeri değiştiremeyiz
+console.log(euroDun);
+
+//array
+let konutKredileri = ["Konut kredisi", "Emlak konut kredisi", "Kamu Konut Kredisi"];
+
+console.log("<ıul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+  console.log("<li>" + konutKredileri[i] + "</li>");
+}
+console.log(konutKredileri);
